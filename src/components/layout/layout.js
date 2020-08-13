34c7ebc56e3daa6d994162628919d2375b007958
adamcoward <adam@cowsystems.com>
2020-08-14 00:45:20 +0100

    Removed Headroom component.

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,7 +2,6 @@ import tw from "twin.macro"
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
-import Headroom from "react-headroom"
 
 import "../../../node_modules/slick-carousel/slick/slick.css"
 import "../../../node_modules/slick-carousel/slick/slick-theme.css"
@@ -15,9 +14,7 @@ export default function Layout({ children }) {
   return (
     <div>
 
-    <Headroom>
       <Header></Header>
-    </Headroom>  
      
         
         <SMNLayout>
