cdec353b44eb5ccd8c50c98936e286d0712c810a
Bekah McDonald <git@bekahmcdonald.com>
2020-12-12 14:50:37 +0000

    Add a resolve alias to webpack to avoid relative paths (eg ../components)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   // Hack due to Tailwind ^1.1.0 using `reduce-css-calc` which assumes node
   // https://github.com/bradlc/babel-plugin-tailwind-components/issues/39#issuecomment-526892633
@@ -5,4 +7,11 @@ exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
   config.node = {
     fs: "empty",
   }
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src")
+      }
+    }
+  });
 }
