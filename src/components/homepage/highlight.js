ca06b66213e870df5d7aacb3b0dab7020707beee
Bekah McDonald <git@bekahmcdonald.com>
2020-12-15 19:05:34 +0000

    Restyle homepage Highlight component

--- src/components/homepage/highlight.js
+++ src/components/homepage/highlight.js
@@ -1,8 +1,6 @@
 import tw from "twin.macro"
 import React from "react"
 import { RichText } from 'prismic-reactjs'
-
-import HomeAnimation from "./animation"
 import { Link } from "gatsby"
 
 const Hero = tw.div`
@@ -12,109 +10,102 @@ const Hero = tw.div`
     mt-12
 `
 
-const ImageWrap = tw.div `
+const ImageWrap = tw.div`
     overflow-hidden
     h-full
+    mb-3
 `
 
-const BgImage =  tw.img ` 
+const BgImage = tw.img` 
     h-sm md:h-auto
     w-full
 `
 
-const Wrapper = tw.div `
+const Wrapper = tw.div`
     w-full
     mx-auto
-
 `
 
-const Info = tw.div `
+const Info = tw.div`
     w-full
-    text-black
     font-sans
+    max-w-xl
 `
 
-const Title = tw.div `
+const Title = tw.span`
     font-title
-    text-2xl md:text-4xl
+    text-2xl md:text-3xl
     leading-normal
     uppercase
     font-bold
     mb-2
+    text-purple
 `
 
-const Subhead = tw.div `
-    text-xl md:text-2xl
-    leading-tight
-    text-midgrey
+const Subhead = tw.span`
+    text-2xl md:text-3xl
+    leading-10
+    tracking-tight
+    font-normal
 `
 
-const RowTitle = tw.div `
+const RowTitle = tw.div`
     mb-4 md:mb-8
     flex
     pb-4
     items-center
 `
 
-const RowTitleMain = tw.div `
+const RowTitleMain = tw.div`
     font-title
-    font-semibold
-    text-2xl md:text-4xl
+    font-bold
+    text-4xl md:text-6xl
     leading-normal
     flex-grow
+    uppercase
 `
 
-const RowTitleLink = tw.div `
-    pt-2 md:pt-4
-    text-lg 
-    md:text-xl
-    leading-normal
-    hover:underline
-`
-
-const HoverWrap = tw.div `
+const HoverWrap = tw.div`
     hover:opacity-75 
 `
 
 
-export default function HomeHighlight ( {data} ) {
+export default function HomeHighlight({ data }) {
 
     return (
 
-    <Hero>
+        <Hero>
 
-    <RowTitle>
+            <RowTitle>
 
-        <RowTitleMain>Changework Projects</RowTitleMain>
-            <RowTitleLink>
-                <Link to={`/changework`}>See all &rarr;</Link>     
-        </RowTitleLink>
+                <RowTitleMain>Projects</RowTitleMain>
 
-    </RowTitle>
+            </RowTitle>
 
-    <Link to={`/changework/${data.project._meta.uid}`}>
-        
-        <HoverWrap>
+            <Link to={`/changework/${data.project._meta.uid}`}>
 
-        <ImageWrap>
-            <BgImage src={data.project.lead_image.url}></BgImage>
-        </ImageWrap>
+                <HoverWrap>
+
+                    <ImageWrap>
+                        <BgImage src={data.project.lead_image.url} />
+                    </ImageWrap>
 
 
-        <Wrapper>
-            <Info>
-                <Title>
-                    <RichText render={data.project.title} />
-                </Title>
-                <Subhead>
-                    <RichText render={data.project.challenge} />
-                </Subhead>
-            </Info>
-        </Wrapper>
-        </HoverWrap>
+                    <Wrapper>
+                        <Info>
+                            <Title>
+                                {RichText.asText(data.project.title)}
+                            </Title>
+                            <Subhead>
+                                <span>&thinsp;—&thinsp;</span>
+                                {RichText.asText(data.project.challenge)}
+                            </Subhead>
+                        </Info>
+                    </Wrapper>
+                </HoverWrap>
 
-        </Link>
-    </Hero>
+            </Link>
+        </Hero>
 
     )
 
