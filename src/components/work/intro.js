d0aa80faa291040952b9fadd359f66688af96426
Bekah McDonald <git@bekahmcdonald.com>
2020-12-15 21:26:07 +0000

    Add new Intro component to Work page

---
+++ src/components/work/intro.js
@@ -0,0 +1,33 @@
+import tw from "twin.macro"
+import React from "react"
+
+const Container = tw.div`
+  container
+  mx-auto
+  mt-12 lg:my-20
+`
+
+const Title = tw.h1`
+  font-title
+  text-5xl md:text-7xl lg:text-8xl
+  font-bold
+  lg:w-1/2
+  uppercase
+  leading-none
+  mb-4 md:mb-6 lg:mb-8
+`
+
+const Text = tw.p`
+  text-lg md:text-2xl lg:text-3xl
+  md:w-2/3 lg:w-1/2
+`
+
+export default function Intro() {
+
+  return (
+    <Container>
+      <Title>CHANGEWORK PROJECTS</Title>
+      <Text>We help leading organisations tackle strategic challenges and thrive in a changing world </Text>
+    </Container>
+  )
+}
--- src/pages/changework/index.js
+++ src/pages/changework/index.js
@@ -2,9 +2,10 @@ import React from "react"
 import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 
-import Layout from "../../components/layout/layout"
+import Layout from "~/components/layout/layout"
 
-import ProjectGrid from "../../components/work/projectGrid"
+import Intro from '~/components/work/intro';
+import ProjectGrid from "~/components/work/projectGrid"
 
 
 export const query = graphql`
@@ -30,28 +31,29 @@ query ChangeworkQuery {
 `;
 
 
-export default function ProjectsIndex ({ data }) {
+export default function ProjectsIndex({ data }) {
 
-    const projects = data.prismic.changework_page.projects
-    if (!projects) return null
+  const projects = data.prismic.changework_page.projects
+  if (!projects) return null
 
-    return (
-       <Layout>
-         
+  return (
+    <Layout>
+
       <Helmet>
-          <title>Changework &mdash; Something More Near</title>
-          <link rel="canonical" href={"https://www.somethingmorenear.com/changework"} />
-          <meta name="title" property="og:title" content={"Changework – Something More Near"} />
+        <title>Changework &mdash; Something More Near</title>
+        <link rel="canonical" href={"https://www.somethingmorenear.com/changework"} />
+        <meta name="title" property="og:title" content={"Changework – Something More Near"} />
 
-          <meta name="type" property="og:type" content="article" />
-          <meta name="description" property="og:description" content={"We are specialists in participatory approaches to innovation, strategy and brand experience. We call it social changework."} />
-          <meta name="image" property="og:image" content={"https://www.somethingmorenear.com/meta_logo.png"} />
-          <meta name="url" property="og:url" content={"https://www.somethingmorenear.com"} />
+        <meta name="type" property="og:type" content="article" />
+        <meta name="description" property="og:description" content={"We are specialists in participatory approaches to innovation, strategy and brand experience. We call it social changework."} />
+        <meta name="image" property="og:image" content={"https://www.somethingmorenear.com/meta_logo.png"} />
+        <meta name="url" property="og:url" content={"https://www.somethingmorenear.com"} />
       </Helmet>
 
-        <ProjectGrid data={projects}></ProjectGrid>
-            
-       </Layout>
-    )
+      <Intro />
+      <ProjectGrid data={projects}></ProjectGrid>
+
+    </Layout>
+  )
 
 }
