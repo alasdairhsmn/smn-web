1e16059f624e8414acfd474ca7104b3d613c0fc8
Bekah McDonald <git@bekahmcdonald.com>
2020-12-15 21:55:02 +0000

    Tidy up footer code a little

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -6,7 +6,6 @@ import SMN from "../../assets/SomethingMoreNear.svg"
 
 import LI from "../../assets/icons/linkedin.svg"
 import EM from "../../assets/icons/email.svg"
-import IG from "../../assets/icons/instagram.svg"
 
 const Wrapper = tw.div`
   bg-darkblue
@@ -27,7 +26,7 @@ const Grid = tw.div`
   min-h-screen80
 `
 
-const LogoBlock = tw.div `
+const LogoBlock = tw.div`
   col-span-12 md:col-span-5
   md:border-r-3
   border-white
@@ -40,7 +39,7 @@ const LogoBlock = tw.div `
   justify-self-end
 `
 
-const TextBlock = tw.div `
+const TextBlock = tw.div`
   col-span-6
   col-start-7
   md:flex
@@ -48,7 +47,7 @@ const TextBlock = tw.div `
   flex-wrap
 `
 
-const InnerText = tw.div `
+const InnerText = tw.div`
   w-auto
   mt-12
   mb-8
@@ -57,14 +56,14 @@ const InnerText = tw.div `
   border-white
 `
 
-const MetaLink = tw.a `
+const MetaLink = tw.a`
     hover:underline
     mb-4
     flex
     items-center
 `
 
-const MetaIcon = tw.span `
+const MetaIcon = tw.span`
     w-6
     h-6
     mr-3
@@ -74,57 +73,54 @@ const MetaIcon = tw.span `
 `
 
 
-export default function Footer () {
+export default function Footer() {
 
-    return (
-        <Wrapper>
+  return (
+    <Wrapper>
 
-          <Grid>
+      <Grid>
 
-            <LogoBlock>
+        <LogoBlock>
 
-              <Link css={tw`w-full`} to={'/'}>
-                <SMN css={tw`fill-current text-white`} />
-              </Link>
+          <Link css={tw`w-full`} to={'/'}>
+            <SMN css={tw`fill-current text-white`} />
+          </Link>
 
-            </LogoBlock>
+        </LogoBlock>
 
-            <TextBlock>
+        <TextBlock>
 
+          <div css={tw`w-full lg:max-w-24`}>
 
-            <div css={tw`w-full lg:max-w-24`}> 
+            <MetaLink href="mailto:hello@somethingmorenear.com">
+              <MetaIcon>
+                <EM />
+              </MetaIcon>
+              <span>hello@somethingmorenear.com</span>
+            </MetaLink>
 
-                <MetaLink href="mailto:hello@somethingmorenear.com">
-                      <MetaIcon>
-                      <EM />
-                      </MetaIcon>
-                      <span>hello@somethingmorenear.com</span>
-                </MetaLink>
-                    
-                <MetaLink target="_blank" href="https://www.linkedin.com/company/something-more-near/">
-                    <MetaIcon>
-                    <LI />
-                    </MetaIcon>
-                    <span>LinkedIn</span>
-                </MetaLink>
+            <MetaLink target="_blank" href="https://www.linkedin.com/company/something-more-near/">
+              <MetaIcon>
+                <LI />
+              </MetaIcon>
+              <span>LinkedIn</span>
+            </MetaLink>
 
-            </div>  
+          </div>
 
-            <div css={tw`lg:max-w-24 mt-auto`}>
-              <p css={tw`mb-3 leading-none`}>
-                “The quickest way to find out how something works is to try to change it”
+          <div css={tw`lg:max-w-24 mt-auto`}>
+            <p css={tw`mb-3 leading-none`}>
+              “The quickest way to find out how something works is to try to change it”
               </p>
-              <p>Nabeel Hamdi</p>
-            </div>
+            <p>Nabeel Hamdi</p>
+          </div>
 
 
-            </TextBlock>
+        </TextBlock>
 
-          </Grid>
+      </Grid>
 
-
-
-        </Wrapper>
-    )
+    </Wrapper>
+  )
 
 }
